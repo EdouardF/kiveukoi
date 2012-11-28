f55a9bf01b6277651bf563f181396304fac310fd
tarteAuxPommes <usig@ubu10-MOD.(none)>
2012-11-28 17:49:23 +0100

    Accueil

--- src/com/kiveukoi/MainActivity.java
+++ src/com/kiveukoi/MainActivity.java
@@ -34,31 +34,31 @@ public class MainActivity extends Activity implements OnClickListener {
 	 */
 	@Override
 	public void onClick(View v) {
-<<<<<<< HEAD
 		if (v == btnConnexion) {
-			Intent monIntent = new Intent(this,Accueil.class);
+			Intent monIntent = new Intent(this, Accueil.class);
 			startActivity(monIntent);
-=======
-		EditText editText1 = (EditText)findViewById(R.id.editText1);
-		String content1 = editText1.getText().toString();
-		
-		EditText editText2 = (EditText)findViewById(R.id.editText2);
-		String content2 = editText2.getText().toString();
-		
-		EditText editText3 = (EditText)findViewById(R.id.editText3);
-		String content3 = editText3.getText().toString();
-		
-		EditText editText4 = (EditText)findViewById(R.id.editText4);
-		String content4 = editText4.getText().toString();
-		
-		if (!content1.matches("") && !content2.matches("") && !content3.matches("") && !content4.matches("")) {
-			if (v == btnConnexion) {
-				Intent monIntent = new Intent(this,Accueil.class);
-				startActivity(monIntent);
+			EditText editText1 = (EditText) findViewById(R.id.editText1);
+			String content1 = editText1.getText().toString();
+
+			EditText editText2 = (EditText) findViewById(R.id.editText2);
+			String content2 = editText2.getText().toString();
+
+			EditText editText3 = (EditText) findViewById(R.id.editText3);
+			String content3 = editText3.getText().toString();
+
+			EditText editText4 = (EditText) findViewById(R.id.editText4);
+			String content4 = editText4.getText().toString();
+
+			if (!content1.matches("") && !content2.matches("")
+					&& !content3.matches("") && !content4.matches("")) {
+				if (v == btnConnexion) {
+					Intent monIntent1 = new Intent(this, Accueil.class);
+					startActivity(monIntent1);
+				}
+			} else {
+				Toast.makeText(this, "Code incorrect", Toast.LENGTH_LONG)
+						.show();
 			}
-		} else {
-			Toast.makeText(this, "Code incorrect", Toast.LENGTH_LONG).show();
->>>>>>> 730ea0cb518f399ca76ccde6a3b7d7ef1b964dd5
 		}
 	}
 
@@ -67,5 +67,5 @@ public class MainActivity extends Activity implements OnClickListener {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-	
+
 }
