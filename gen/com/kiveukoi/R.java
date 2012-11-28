1d048b118e7afda43f6047d1905eb5a8c5df618a
Benjamin Humeau <humeau-benjamin@orange.fr>
2012-11-28 17:51:24 +0100

    Ajout de la page de login

--- gen/com/kiveukoi/R.java
+++ gen/com/kiveukoi/R.java
@@ -14,46 +14,64 @@ public final class R {
         public static final int logo=0x7f020000;
     }
     public static final class id {
-        public static final int btnConnexion=0x7f070008;
+        public static final int btnConnexion=0x7f070010;
         public static final int email=0x7f070003;
+        public static final int lblChampPassword=0x7f07000f;
+        public static final int lblCodePin=0x7f07000e;
+        public static final int lblLogin=0x7f070008;
         public static final int login=0x7f070005;
         public static final int login_form=0x7f070002;
         public static final int login_status=0x7f070000;
         public static final int login_status_message=0x7f070001;
-        public static final int menu_forgot_password=0x7f07000a;
-        public static final int menu_settings=0x7f070009;
+        public static final int menu_forgot_password=0x7f070013;
+        public static final int menu_settings=0x7f070012;
         public static final int password=0x7f070004;
         public static final int sign_in_button=0x7f070006;
-        public static final int txtPasswordNum=0x7f070007;
+        public static final int txtMail=0x7f070007;
+        public static final int txtPIN1=0x7f07000b;
+        public static final int txtPIN2=0x7f07000a;
+        public static final int txtPIN3=0x7f07000c;
+        public static final int txtPIN4=0x7f07000d;
+        public static final int txtPassword=0x7f070009;
+        public static final int txtPasswordNum=0x7f070011;
     }
     public static final class layout {
         public static final int activity_accueil=0x7f030000;
         public static final int activity_identification=0x7f030001;
-        public static final int activity_main=0x7f030002;
+        public static final int activity_login_web_service=0x7f030002;
+        public static final int activity_main=0x7f030003;
     }
     public static final class menu {
         public static final int activity_accueil=0x7f060000;
         public static final int activity_identification=0x7f060001;
-        public static final int activity_main=0x7f060002;
+        public static final int activity_login_web_service=0x7f060002;
+        public static final int activity_main=0x7f060003;
     }
     public static final class string {
-        public static final int action_sign_in_register=0x7f040007;
-        public static final int action_sign_in_short=0x7f040008;
+        public static final int action_sign_in_register=0x7f04000e;
+        public static final int action_sign_in_short=0x7f04000f;
         public static final int app_name=0x7f040000;
-        public static final int error_field_required=0x7f04000e;
-        public static final int error_incorrect_password=0x7f04000d;
-        public static final int error_invalid_email=0x7f04000b;
-        public static final int error_invalid_password=0x7f04000c;
+        public static final int error_field_required=0x7f040015;
+        public static final int error_incorrect_password=0x7f040014;
+        public static final int error_invalid_email=0x7f040012;
+        public static final int error_invalid_password=0x7f040013;
         public static final int hello_world=0x7f040001;
-        public static final int login_progress_signing_in=0x7f04000a;
-        public static final int menu_forgot_password=0x7f040009;
+        public static final int hint_mail=0x7f040009;
+        public static final int hint_password=0x7f04000a;
+        public static final int login_progress_signing_in=0x7f040011;
+        public static final int menu_forgot_password=0x7f040010;
         public static final int menu_settings=0x7f040002;
+        public static final int nom_bouton_connexion=0x7f040007;
+        public static final int nom_champ_adresse_mail=0x7f040005;
+        public static final int nom_champ_password=0x7f040006;
+        public static final int nom_champ_pin=0x7f040008;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f040005;
-        public static final int prompt_password=0x7f040006;
+        public static final int prompt_email=0x7f04000c;
+        public static final int prompt_password=0x7f04000d;
         public static final int title_activity_accueil=0x7f040003;
-        public static final int title_activity_identification=0x7f040004;
+        public static final int title_activity_identification=0x7f04000b;
+        public static final int title_activity_login_web_service=0x7f040004;
     }
     public static final class style {
         /** 
