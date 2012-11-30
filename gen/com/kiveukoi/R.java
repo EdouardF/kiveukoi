85401457824b7b744a8d04e18a2cc2fabc87e0e2
tarteAuxPommes <usig@ubu10-MOD.(none)>
2012-11-30 10:28:22 +0100

    Id

--- gen/com/kiveukoi/R.java
+++ gen/com/kiveukoi/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class id {
+        public static final int ancienCode=0x7f060024;
         public static final int btnAjout=0x7f060003;
         public static final int btnConnexion=0x7f06001f;
         public static final int btnProfil=0x7f060002;
@@ -29,14 +30,16 @@ public final class R {
         public static final int login_form=0x7f060019;
         public static final int login_status=0x7f060017;
         public static final int login_status_message=0x7f060018;
-        public static final int menu_forgot_password=0x7f060025;
-        public static final int menu_settings=0x7f060024;
+        public static final int menu_forgot_password=0x7f060028;
+        public static final int menu_settings=0x7f060027;
         public static final int neufh=0x7f060005;
+        public static final int nouveauCode=0x7f060026;
         public static final int onzeh=0x7f060007;
         public static final int password=0x7f06001b;
         public static final int progressBar1=0x7f06000f;
         public static final int quatorzeh=0x7f06000a;
         public static final int quinzeh=0x7f06000b;
+        public static final int repeterCode=0x7f060025;
         public static final int seizeh=0x7f06000c;
         public static final int sign_in_button=0x7f06001d;
         public static final int tableRow1=0x7f060000;
@@ -68,8 +71,9 @@ public final class R {
     }
     public static final class string {
         public static final int TitreAjoutModif=0x7f030009;
-        public static final int action_sign_in_register=0x7f03002a;
-        public static final int action_sign_in_short=0x7f03002b;
+        public static final int action_sign_in_register=0x7f03002d;
+        public static final int action_sign_in_short=0x7f03002e;
+        public static final int ancienCode=0x7f030027;
         public static final int app_name=0x7f030000;
         public static final int btnajouter=0x7f030008;
         public static final int btnprofil=0x7f030006;
@@ -79,10 +83,10 @@ public final class R {
         public static final int dixhuith=0x7f030026;
         public static final int dixsepth=0x7f030025;
         public static final int douzeh=0x7f030020;
-        public static final int error_field_required=0x7f030031;
-        public static final int error_incorrect_password=0x7f030030;
-        public static final int error_invalid_email=0x7f03002e;
-        public static final int error_invalid_password=0x7f03002f;
+        public static final int error_field_required=0x7f030034;
+        public static final int error_incorrect_password=0x7f030033;
+        public static final int error_invalid_email=0x7f030031;
+        public static final int error_invalid_password=0x7f030032;
         public static final int groupe=0x7f030013;
         public static final int hello_world=0x7f030001;
         public static final int heureDebut=0x7f03000e;
@@ -91,8 +95,8 @@ public final class R {
         public static final int hint_password=0x7f03001b;
         public static final int huith=0x7f03001c;
         public static final int login=0x7f03000b;
-        public static final int login_progress_signing_in=0x7f03002d;
-        public static final int menu_forgot_password=0x7f03002c;
+        public static final int login_progress_signing_in=0x7f030030;
+        public static final int menu_forgot_password=0x7f03002f;
         public static final int menu_settings=0x7f030002;
         public static final int neufh=0x7f03001d;
         public static final int nom_bouton_connexion=0x7f030018;
@@ -100,20 +104,22 @@ public final class R {
         public static final int nom_champ_password=0x7f030017;
         public static final int nom_champ_pin=0x7f030019;
         public static final int nombrePoste=0x7f030014;
+        public static final int nouveauCode=0x7f030028;
         public static final int onzeh=0x7f03001f;
         public static final int processeur=0x7f030011;
         public static final int professeur=0x7f030012;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f030028;
-        public static final int prompt_password=0x7f030029;
+        public static final int prompt_email=0x7f03002b;
+        public static final int prompt_password=0x7f03002c;
         public static final int quatorzeh=0x7f030022;
         public static final int quinzeh=0x7f030023;
         public static final int ram=0x7f030010;
+        public static final int repeterCode=0x7f030029;
         public static final int seizeh=0x7f030024;
         public static final int title_activity_accueil=0x7f030003;
         public static final int title_activity_ajout_modif=0x7f03000a;
-        public static final int title_activity_identification=0x7f030027;
+        public static final int title_activity_identification=0x7f03002a;
         public static final int title_activity_login_web_service=0x7f030015;
         public static final int title_activity_profil=0x7f030005;
         public static final int title_activity_statistiques=0x7f030004;
---
+++ src/com/kiveukoi/MainActivity.java
@@ -0,0 +1,126 @@
+package com.kiveukoi;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener {
+    Button btnConnexion = null;
+
+	/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_main);
+		btnConnexion = (Button) findViewById(R.id.btnConnexion);
+
+		btnConnexion.setOnClickListener(this);
+		
+		/**
+		 * passer d'une case Ã  l'autre pour la saisie du code PIN
+		 */
+		EditText editText1 = (EditText)findViewById(R.id.editText1);
+		EditText editText2 = (EditText)findViewById(R.id.editText2);
+		EditText editText3 = (EditText)findViewById(R.id.editText3);
+		editText1.addTextChangedListener(new TextWatcher() {
+		    @Override
+		    public void afterTextChanged(Editable s) {
+		    	EditText editText2 = (EditText)findViewById(R.id.editText2);
+		    	editText2.requestFocus();
+		    }
+
+		    @Override
+		    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+		        // TODO Auto-generated method stub
+		    }
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				EditText editText2 = (EditText)findViewById(R.id.editText2);
+		    	editText2.requestFocus();
+			}
+		});
+		editText2.addTextChangedListener(new TextWatcher() {
+		    @Override
+		    public void afterTextChanged(Editable s) {
+		    	EditText editText3 = (EditText)findViewById(R.id.editText3);
+		    	editText3.requestFocus();
+		    }
+
+		    @Override
+		    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+		        // TODO Auto-generated method stub
+		    }
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				EditText editText3 = (EditText)findViewById(R.id.editText3);
+		    	editText3.requestFocus();
+			}
+		});
+		editText3.addTextChangedListener(new TextWatcher() {
+		    @Override
+		    public void afterTextChanged(Editable s) {
+		    	EditText editText4 = (EditText)findViewById(R.id.editText4);
+		    	editText4.requestFocus();
+		    }
+
+		    @Override
+		    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+		        // TODO Auto-generated method stub
+		    }
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				EditText editText4 = (EditText)findViewById(R.id.editText4);
+		    	editText4.requestFocus();
+			}
+		});
+	}
+	
+	/**
+	 * VÃ©rifie que le code PIN est renseignÃ© pour accÃ©der Ã  l'accueil
+	 */
+	@Override
+	public void onClick(View v) {
+		EditText editText1 = (EditText)findViewById(R.id.editText1);
+		String content1 = editText1.getText().toString();
+
+		EditText editText2 = (EditText)findViewById(R.id.editText2);
+		String content2 = editText2.getText().toString();
+		
+		EditText editText3 = (EditText)findViewById(R.id.editText3);
+		String content3 = editText3.getText().toString();
+		
+		EditText editText4 = (EditText)findViewById(R.id.editText4);
+		String content4 = editText4.getText().toString();
+		
+		if (!content1.matches("") && !content2.matches("") && !content3.matches("") && !content4.matches("")) {
+			if (v == btnConnexion) {
+				Intent monIntent = new Intent(this,Accueil.class);
+				startActivity(monIntent);
+			}
+		} else {
+			Toast.makeText(this, "Code incorrect", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+	
+}
