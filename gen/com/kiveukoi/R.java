4ebfa15c86779cdd53d25686b0ea449a87d67f30
Benjamin Humeau <humeau-benjamin@orange.fr>
2012-12-06 08:37:57 +0100

    Modification de la page de connexion

--- gen/com/kiveukoi/R.java
+++ gen/com/kiveukoi/R.java
@@ -15,32 +15,27 @@ public final class R {
     }
     public static final class id {
         public static final int btnConnexion=0x7f070001;
-        public static final int btnLogin=0x7f07000c;
-        public static final int editText1=0x7f070015;
-        public static final int editText2=0x7f070016;
-        public static final int editText3=0x7f070017;
-        public static final int editText4=0x7f070018;
-        public static final int email=0x7f070010;
-        public static final int lblLogin=0x7f070002;
-        public static final int lblPassword=0x7f070004;
-        public static final int lblPin=0x7f070006;
-        public static final int login=0x7f070012;
-        public static final int login_form=0x7f07000f;
-        public static final int login_status=0x7f07000d;
-        public static final int login_status_message=0x7f07000e;
-        public static final int menu_forgot_password=0x7f07001a;
-        public static final int menu_settings=0x7f070019;
-        public static final int password=0x7f070011;
-        public static final int relativeLayout1=0x7f070007;
-        public static final int sign_in_button=0x7f070013;
+        public static final int btnLogin=0x7f070006;
+        public static final int editText1=0x7f070010;
+        public static final int editText2=0x7f070011;
+        public static final int editText3=0x7f070012;
+        public static final int editText4=0x7f070013;
+        public static final int email=0x7f07000b;
+        public static final int login=0x7f07000d;
+        public static final int login_form=0x7f07000a;
+        public static final int login_status=0x7f070008;
+        public static final int login_status_message=0x7f070009;
+        public static final int menu_forgot_password=0x7f070015;
+        public static final int menu_settings=0x7f070014;
+        public static final int password=0x7f07000c;
+        public static final int sign_in_button=0x7f07000e;
         public static final int tableRow1=0x7f070000;
-        public static final int textView1=0x7f070014;
-        public static final int txtLogin=0x7f070003;
-        public static final int txtPassword=0x7f070005;
-        public static final int txtPin1=0x7f070008;
-        public static final int txtPin2=0x7f070009;
-        public static final int txtPin3=0x7f07000a;
-        public static final int txtPin4=0x7f07000b;
+        public static final int textCodeSecret=0x7f070007;
+        public static final int textView1=0x7f07000f;
+        public static final int txtLogin=0x7f070002;
+        public static final int txtPassword=0x7f070003;
+        public static final int txtSecret1=0x7f070005;
+        public static final int txtSecret2=0x7f070004;
     }
     public static final class layout {
         public static final int activity_accueil=0x7f030000;
@@ -55,29 +50,30 @@ public final class R {
         public static final int activity_main=0x7f060003;
     }
     public static final class string {
-        public static final int action_sign_in_register=0x7f04000e;
-        public static final int action_sign_in_short=0x7f04000f;
+        public static final int action_sign_in_register=0x7f04000f;
+        public static final int action_sign_in_short=0x7f040010;
         public static final int app_name=0x7f040000;
-        public static final int error_field_required=0x7f040015;
-        public static final int error_incorrect_password=0x7f040014;
-        public static final int error_invalid_email=0x7f040012;
-        public static final int error_invalid_password=0x7f040013;
+        public static final int error_field_required=0x7f040016;
+        public static final int error_incorrect_password=0x7f040015;
+        public static final int error_invalid_email=0x7f040013;
+        public static final int error_invalid_password=0x7f040014;
         public static final int hello_world=0x7f040001;
-        public static final int hint_login=0x7f040008;
-        public static final int label_login=0x7f040007;
-        public static final int label_password=0x7f040009;
-        public static final int label_pin=0x7f04000a;
+        public static final int hint_login=0x7f040007;
+        public static final int hint_password=0x7f040008;
+        public static final int hint_secret=0x7f040009;
+        public static final int hint_secretRepeter=0x7f04000a;
+        public static final int label_secret=0x7f04000b;
         public static final int login=0x7f040004;
-        public static final int login_progress_signing_in=0x7f040011;
-        public static final int menu_forgot_password=0x7f040010;
+        public static final int login_progress_signing_in=0x7f040012;
+        public static final int menu_forgot_password=0x7f040011;
         public static final int menu_settings=0x7f040002;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f04000c;
-        public static final int prompt_password=0x7f04000d;
+        public static final int prompt_email=0x7f04000d;
+        public static final int prompt_password=0x7f04000e;
         public static final int title_activity_accueil=0x7f040003;
         public static final int title_activity_connexion=0x7f040006;
-        public static final int title_activity_identification=0x7f04000b;
+        public static final int title_activity_identification=0x7f04000c;
         public static final int title_pin=0x7f040005;
     }
     public static final class style {
