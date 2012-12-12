b6b39fa34ef1d3208b77e7d50f5d6e03502abd5b
tarteAuxPommes <usig@ubu10-MOD.(none)>
2012-12-12 14:00:51 +0100

    initR

--- src/com/kiveukoi/Accueil.java
+++ src/com/kiveukoi/Accueil.java
@@ -7,11 +7,23 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class Accueil extends Activity implements OnClickListener{
 	Button btnAjout = null;
 	Button btnStatistiques = null;
 	Button btnProfil = null;
+	ProgressBar progressBar1 = null;
+	ProgressBar progressBar2 = null;
+	ProgressBar progressBar3 = null;
+	ProgressBar progressBar4 = null;
+	ProgressBar progressBar5 = null;
+	ProgressBar progressBar6 = null;
+	ProgressBar progressBar7 = null;
+	ProgressBar progressBar8 = null;
+	ProgressBar progressBar9 = null;
+	ProgressBar progressBar10 = null;
+	ProgressBar progressBar11 = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,6 +34,28 @@ public class Accueil extends Activity implements OnClickListener{
 		btnStatistiques.setOnClickListener(this);
 		btnProfil = (Button) findViewById(R.id.btnProfil);
 		btnProfil.setOnClickListener(this);
+		progressBar1 = (ProgressBar) findViewById(R.id.progressBar1);
+		progressBar1.setOnClickListener(this);
+		progressBar2 = (ProgressBar) findViewById(R.id.progressBar2);
+		progressBar2.setOnClickListener(this);
+		progressBar3 = (ProgressBar) findViewById(R.id.progressBar3);
+		progressBar3.setOnClickListener(this);
+		progressBar4 = (ProgressBar) findViewById(R.id.progressBar4);
+		progressBar4.setOnClickListener(this);
+		progressBar5 = (ProgressBar) findViewById(R.id.progressBar5);
+		progressBar5.setOnClickListener(this);
+		progressBar6 = (ProgressBar) findViewById(R.id.progressBar6);
+		progressBar6.setOnClickListener(this);
+		progressBar7 = (ProgressBar) findViewById(R.id.progressBar7);
+		progressBar7.setOnClickListener(this);
+		progressBar8 = (ProgressBar) findViewById(R.id.progressBar8);
+		progressBar8.setOnClickListener(this);
+		progressBar9 = (ProgressBar) findViewById(R.id.progressBar9);
+		progressBar9.setOnClickListener(this);
+		progressBar10 = (ProgressBar) findViewById(R.id.progressBar10);
+		progressBar10.setOnClickListener(this);
+		progressBar11 = (ProgressBar) findViewById(R.id.progressBar11);
+		progressBar11.setOnClickListener(this);
 
 	}
 
@@ -44,6 +78,50 @@ public class Accueil extends Activity implements OnClickListener{
 		}
 		if (v == btnProfil) {
 			Intent monIntent = new Intent(this,Profil.class);
+			startActivity(monIntent);
+		}
+		if (v == progressBar1) {
+			Intent monIntent = new Intent(this,AjoutModif.class);
+			startActivity(monIntent);
+		}
+		if (v == progressBar2) {
+			Intent monIntent = new Intent(this,AjoutModif.class);
+			startActivity(monIntent);
+		}
+		if (v == progressBar3) {
+			Intent monIntent = new Intent(this,AjoutModif.class);
+			startActivity(monIntent);
+		}
+		if (v == progressBar4) {
+			Intent monIntent = new Intent(this,AjoutModif.class);
+			startActivity(monIntent);
+		}
+		if (v == progressBar5) {
+			Intent monIntent = new Intent(this,AjoutModif.class);
+			startActivity(monIntent);
+		}
+		if (v == progressBar6) {
+			Intent monIntent = new Intent(this,AjoutModif.class);
+			startActivity(monIntent);
+		}
+		if (v == progressBar7) {
+			Intent monIntent = new Intent(this,AjoutModif.class);
+			startActivity(monIntent);
+		}
+		if (v == progressBar8) {
+			Intent monIntent = new Intent(this,AjoutModif.class);
+			startActivity(monIntent);
+		}
+		if (v == progressBar9) {
+			Intent monIntent = new Intent(this,AjoutModif.class);
+			startActivity(monIntent);
+		}
+		if (v == progressBar10) {
+			Intent monIntent = new Intent(this,AjoutModif.class);
+			startActivity(monIntent);
+		}
+		if (v == progressBar11) {
+			Intent monIntent = new Intent(this,AjoutModif.class);
 			startActivity(monIntent);
 		}
 	}
